f6cee8832a0c4d1fc53c46d1ff1d8ccff17e6a0f
Tore Saeterdal <torees@stud.ntnu.no>
2016-01-26 12:33:28 +0100

    started on udp receive

--- Ex3/UDP.go
+++ Ex3/UDP.go
@@ -2,7 +2,39 @@ package main
 	
 import (
 	"fmt"
-	"time"
+	
+	"net"
+	"os"
 )
 
-var buffer [1024]byte
+
+
+func CheckForError(err error){
+	if err != nil {  //nil is default error from "net" package
+		fmt.Println("Error in connecting to port:",err)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	//connect to port 30000
+	adress,err := net.ResolveUDPAddr("udp",":30000")
+	CheckForError(err)
+
+	// listen at port
+	connection,err := net.ListenUDP("udp",adress)
+	CheckForError(err)
+	defer connection.Close()
+
+	buffer := make([]byte,1024)
+
+	for{
+		n,adress,err :=  connection.ReadFromUDP(buffer)
+		fmt.Println("Receiving: ",string(buffer[0:n]), " from ", adress)
+
+		if err != nil{
+			fmt.Println("Error: ",err)
+		}
+	}
+
+}
