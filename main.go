90a866236cd8a8af72a1e9f9cb9faa799aef08f4
Student <student@itk-d650>
2016-02-17 15:05:45 +0100

    error handling

--- main.go
+++ main.go
@@ -13,43 +13,48 @@ import (
 
 func main(){
 	master := false
-	ping := false
+	
 	pingPort := ":50001"
 	countValuePort := ":50002"
 	pingConn := ClientConnectUDP(pingPort)
 	countConn := ClientConnectUDP(countValuePort)
-	tickChan := make(chan int)
-
-	pingRecv:= make(chan int)
-	valueChan := make(chan int)
 	
-	go checkPing(pingPort,pingRecv)
+	timeOut:= make(chan int)
+	pingRecv:= make(chan int)
+
+
+	
+	//valueChan := make(chan int)
+	
+	go checkPing(pingPort,pingRecv,timeOut)
 	i:= 0
-	cmd:= exec.Command("gnome-terminal","-x", "/home/student/Documents/Sanntid/go run main.go")
+	cmd:= exec.Command("gnome-terminal","-x","go run main.go")
 	
 	for !master{
+
 		select{
-			case <-timestamp:
-				fmt.Println("timestamp")
-				if ping==false{
-					fmt.Println("Dobby is slave no more! ")
-					master = true
-				}
-				ping=false
+			case <-timeOut:		
+				fmt.Println("Dobby is slave no more! ")
+				master = true
+				
+				
 				//timeout
 			case <- pingRecv:
-				ping = true
+				fmt.Println("tick")
+				
 			
 
 			default:
-				i=<-valueChan
+				//fmt.Println("still slave")
+				//i=<-valueChan
 
 
 		}
 	}
 
+
 	go pingThread(pingConn)
-	cmd.Start()
+	cmd.Output()
 	for{
 		fmt.Println(i)
 		ClientSend(i,countConn)
@@ -79,6 +84,7 @@ func checkCountVal(valueChan chan int,countValuePort string){
 func ClientConnectUDP(port string) *net.UDPConn{
 	adress,_ :=net.ResolveUDPAddr("udp","129.241.187.20"+port)
 	conn,_:= net.DialUDP("udp",nil,adress)
+
 	return conn	
 }
 
@@ -98,14 +104,24 @@ func pingThread(pingConn *net.UDPConn){
 	}
 }
 
-func checkPing(pingPort string, pingRecv chan int){
+func checkPing(pingPort string, pingRecv chan int,timeOut chan int){
 	ServAddr,_ := net.ResolveUDPAddr("udp",pingPort)
 	ServConn,_ := net.ListenUDP("udp",ServAddr)
+	
+	
 	buf := make([]byte,1024)
 	for{
+		fmt.Println("Dobby is slave! ")
+		ServConn.SetReadDeadline(time.Now().Add(time.Second*1))
 		fmt.Println("Slaving away...")		
-		
-		ServConn.ReadFromUDP(buf)
+	
+		_,_,err:=ServConn.ReadFromUDP(buf)
+		fmt.Println("Dobby NOT slave! ")
+		if err != nil{
+			fmt.Println("error not nil")
+			timeOut<-1
+
+		}
 		pingRecv <- 1
 		//time.Sleep(time.Second*1)
 
