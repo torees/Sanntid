ff4f2e6f5b40995ca6b650817fbcc5f057037558
HAB <kissahen@gmail.com>
2016-03-03 17:55:28 +0100

    started on fsm

--- src/driver/driver.go
+++ src/driver/driver.go
@@ -1,6 +1,5 @@
 package driver
 
-
 /*
 #cgo LDFLAGS: -lcomedi -lpthread -lm
 #cgo CFLAGS: -std=c99
@@ -10,47 +9,43 @@ package driver
 */
 import "C"
 
-type Elevator_dir int
+type Elev_dir int
 type Button_type int
 
 const (
-	DOWN Elevator_dir = -1
-	STOP Elevator_dir = 0
-	UP Elevator_dir = 1
+	DOWN Elev_dir = -1
+	STOP Elev_dir = 0
+	UP   Elev_dir = 1
 )
 
-func ElevatorInit(){
+func ElevInit() {
 	C.elev_init()
 }
 
-func ElevatorStart(dir Elevator_dir){
+func ElevStart(dir Elev_dir) {
 	C.elev_set_motor_direction(C.elev_motor_direction_t(dir))
 }
 
-
-func NetworkDisconnect(value int){
+func NetworkDisconnect(value int) {
 	C.elev_set_stop_lamp(C.int(value))
 }
 
-func ButtonLamp(button Button_type, floor int, value int){
-	C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor),C.int(value))
+func ButtonLamp(button Button_type, floor int, value int) {
+	C.elev_set_button_lamp(C.elev_button_type_t(button), C.int(floor), C.int(value))
 }
 
-func DoorOpen(value int ){
+func DoorOpen(value int) {
 	C.elev_set_door_open_lamp(C.int(value))
 }
 
-func FloorIndicator(floor int){
+func FloorIndicator(floor int) {
 	C.elev_set_floor_indicator(C.int(floor))
 }
 
-func FloorSensor() int{
+func FloorSensor() int {
 	return int(C.elev_get_floor_sensor_signal())
 }
 
-func ButtonPushed(button Button_type, floor int) int{
-	return int(C.elev_get_button_signal(C.elev_button_type_t(button),C.int(floor)))
+func ButtonPushed(button Button_type, floor int) int {
+	return int(C.elev_get_button_signal(C.elev_button_type_t(button), C.int(floor)))
 }
-
-
-
--- src/statemachine/statemachine.go
+++ src/statemachine/statemachine.go
@@ -11,8 +11,7 @@ type State int
 const N_FLOORS = 4
 const (
 	idle State = iota
-	down
-	up
+	running
 	doorOpen
 )
 
@@ -23,30 +22,67 @@ type LocalQueue struct {
 }
 
 func main() {
-	fmt.Println("hello world")
-	driver.ElevatorInit()
 	var local LocalQueue
-	fmt.Println(local)
-	for {
-		local = CheckOrderButton(local)
-		fmt.Println(local)
-		fmt.Println(driver.ButtonPushed(2, 1))
-		time.Sleep(time.Second * 1)
+	fmt.Println("hello world")
+	driver.ElevInit()
+	positionChan := make(chan int)
+	nextTarget := make(chan int)
+	queueChan := make(chan LocalQueue)
+	go ElevPosition(positionChan)
+	go CheckOrderButton(queueChan)
 
+	fstate := idle
+	for {
+		ElevManager(local, fstate, positionChan, nextTarget, queueChan)
 	}
+
 }
-func ElevatorManager() {
-	fmt.Println("hello world")
-	var local LocalQueue
-	fmt.Println(local)
-	local = CheckOrderButton(local)
-	fmt.Println(local)
+func ElevManager(local LocalQueue, fstate State, positionChan chan int, nextTarget chan int, queueChan chan LocalQueue) {
+
+	//local = <-queueChan
+	//fmt.Println(local)
+	//fmt.Println(driver.ButtonPushed(2, 1))
+	//time.Sleep(time.Second * 1)
+	//Lights(local)
+	//var target int
+
+	fmt.Println(<-positionChan)
+	/*
+		switch fstate {
+
+		case idle:
+			target = <-nextTarget
+			position := <-positionChan
+			dir := Sign(target - position)
+			driver.ElevStart(driver.Elev_dir(dir))
+			fstate = running
+			break
+
+		case running:
+			target = <-nextTarget
+			position := <-positionChan
+			if position == target {
+				driver.ElevStart(0)
+				fstate = idle
+				local.down[position] = 0
+				local.internal[position] = 0
+				break
+			}
+			fmt.Println(position)
+			break
+
+		case doorOpen:
+			fstate = idle
+			time.Sleep(time.Second * 1)
+			break
+
+		}*/
 
 }
 
 //if (driver.ButtonPushed(j,i)){
-func CheckOrderButton(local LocalQueue) LocalQueue {
-
+func CheckOrderButton(queueChan chan LocalQueue) {
+	local := <-queueChan
 	for i := 0; i < N_FLOORS; i++ {
 		for j := 0; j < 3; j++ {
 			//sjekker for manglende knapper i endeetasjer
@@ -72,7 +108,7 @@ func CheckOrderButton(local LocalQueue) LocalQueue {
 		}
 
 	}
-	return local
+	queueChan <- local
 }
 
 func ElevPosition(positionChan chan int) {
@@ -80,17 +116,33 @@ func ElevPosition(positionChan chan int) {
 		floor := driver.FloorSensor()
 		if floor != -1 {
 			positionChan <- floor
-
 		}
 		time.Sleep(time.Millisecond * 40)
 	}
 
 }
 
-func OrderFromLocalQueue() {
+func OrderFromLocalQueue(local LocalQueue, nextTarget chan int, queueChan chan LocalQueue) {
+	local = <-queueChan
+	for i := 0; i < 4; i++ {
+		if (local.up[i] != 0) || (local.internal[i] != 0) || (local.down[i] != 0) {
+			nextTarget <- i
+		}
+	}
+	queueChan <- local
+}
+
+func Lights(local LocalQueue) {
 
 }
 
-func Lights() {
+func Sign(val int) int {
+	if val < 0 {
+		return -1
+	} else if val > 0 {
+		return 1
+	} else {
+		return 0
+	}
 
 }
