abe839fd613438798da731ae224b0b874879b846
Mads <YOUR EMAIL ADDRESS>
2016-04-01 18:28:26 +0200

    ping test

--- src/eventhandler/eventhandler.go
+++ src/eventhandler/eventhandler.go
@@ -12,7 +12,7 @@ const(
 	UDPPort = ":20011"
 	)
 
-
+const N_ELEVATORS = 3
 
 func main(){ //function should be renamed afterwards, this is just for testing
 	var myIp string
@@ -25,10 +25,13 @@ func main(){ //function should be renamed afterwards, this is just for testing
 	}
 	fmt.Println("My IP", myIp)
 
+
 	UDPSendMsgChan := make(chan message.UDPMessage,100)
 	UDPPingReceivedChan := make(chan message.UDPMessage,100)
 	UDPOrderReceivedChan := make(chan message.UDPMessage,100)
 	UDPElevatorStateUpdateChan := make(chan message.UDPMessage,100)
+	checkNetworkConChan := make(chan bool)
+
 	//listenPingChan := make(chan bool, 1)
 	// sendElevComChan :=make(chan *net.UDPConn, 10)
 	// listenElevComChan := make(chan *net.UDPConn, 10)
@@ -42,21 +45,46 @@ func main(){ //function should be renamed afterwards, this is just for testing
 
 	go UDPsend(UDPSendConn, UDPSendMsgChan, myIp)
 	go UDPlisten(UDPlistenConn, UDPPingReceivedChan, UDPOrderReceivedChan,UDPElevatorStateUpdateChan)
+	go network.CheckNetworkConnection(checkNetworkConChan)
+	//connectedElevIP := [N_ELEVATORS]string
+
+	connectedElevTimers := make(map[string]*time.Timer)
+
 
 	for{
 		select{
 			case msg := <-UDPPingReceivedChan:
 				fmt.Println("ping received from: ", msg.IP)
+				_,exists := connectedElevTimers[msg.IP]
+
+				if exists{
+					connectedElevTimers[msg.IP].Reset(time.Second)
+				}else{
+					connectedElevTimers[msg.IP] = time.AfterFunc(time.Second, func(){ delete(connectedElevTimers,msg.IP) } )
+				}
+
+
+
+				//add msg.IP to ip list IFNOT there already OR number of elevators = N
+
 			case msg := <- UDPOrderReceivedChan:
 				fmt.Println("order received: ", msg.OrderQueue)
 			case msg := <- UDPElevatorStateUpdateChan:
 				fmt.Println("State update : ", msg.ElevatorStateUpdate)
+			case <- checkNetworkConChan:
+				//network down, handle 
 
 		}
+		
 	}
 	
 
 }
+
+
+
+
+
 
 func UDPsend(conn *net.UDPConn, UDPMsgChan chan message.UDPMessage, IP string){
 	defer conn.Close()
@@ -132,7 +160,6 @@ func UDPlisten(conn *net.UDPConn, UDPPingReceivedChan chan message.UDPMessage, U
 
 	}
 }
-
 
 
 
